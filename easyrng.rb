3f917c12f10aa64398ee09593f51fb863ad76375
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-06-27 12:44:05 +0100

    easyRNG: switch URL to Github releases

--- easyrng.rb
+++ easyrng.rb
@@ -1,7 +1,7 @@
 class Easyrng < Formula
   desc "Random number generators and distributions for C and Fortran"
   homepage "https://tschoonj.github.io/easyRNG"
-  url "https://easyrng.tomschoonjans.eu/easyRNG-1.1.tar.gz"
+  url "https://github.com/tschoonj/easyRNG/releases/download/easyRNG-1.1/easyRNG-1.1.tar.gz"
   sha256 "dd85fc845a5f7e99c71ff440888357304dec938d7be40e5ee4400272d2cb5a41"
 
   depends_on :fortran => [:build, :recommended]
