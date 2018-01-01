6aa719c45efb5b4f23784777b8376a17a45a8c7b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-01-01 14:07:52 +0100

    fgsl: import from homebrew-science

---
+++ fgsl.rb
@@ -0,0 +1,28 @@
+class Fgsl < Formula
+  desc "Fortran bindings for the GNU Scientific Library"
+  homepage "https://www.lrz.de/services/software/mathematik/gsl/fortran/"
+  url "https://www.lrz.de/services/software/mathematik/gsl/fortran/download/fgsl-1.2.0.tar.gz"
+  sha256 "00fd467af2bb778e8d15ac8c27ddc7b9024bb8fa2f950a868d9d24b6086e5ca7"
+
+  depends_on "pkg-config" => :build
+  depends_on :fortran
+  depends_on "gsl"
+
+  def install
+    ENV.deparallelize
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    ENV.fortran
+    system ENV.fc, "#{share}/examples/fgsl/fft.f90",
+                   "-L#{lib}", "-lfgsl", "-I#{include}/fgsl", "-o", "test"
+    system "./test"
+  end
+end
