84c8c2a523e109d14e923dd43a70a5f3bce9c388
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-08 14:20:45 +0000

    gtkmm-plplot 2.3

--- gtkmm-plplot.rb
+++ gtkmm-plplot.rb
@@ -1,8 +1,8 @@
 class GtkmmPlplot < Formula
   desc "Scientific plotting for Gtkmm3"
   homepage "https://tschoonj.github.io/gtkmm-plplot/"
-  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.2/gtkmm-plplot-2.2.tar.gz"
-  sha256 "6b237b461bb205f92e49f744fbd22e4a2299c4b89439d04020117e8a204de3de"
+  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.3/gtkmm-plplot-2.3.tar.gz"
+  sha256 "db2415199b19f3c796000071ba63c3a9e979b8c62a4188777dc06bdee7c44d5d"
 
   depends_on "pkg-config" => :build
   depends_on "gtkmm3"
