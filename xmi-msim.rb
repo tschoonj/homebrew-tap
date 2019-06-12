22357bed17ca08148074ea4bfcac8cf4d6a387c9
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-06-12 09:36:01 +0100

    xmi-msim: build GUI

--- xmi-msim.rb
+++ xmi-msim.rb
@@ -3,16 +3,18 @@ class XmiMsim < Formula
   homepage "https://github.com/tschoonj/xmimsim"
   url "https://xmi-msim.tomschoonjans.eu/xmimsim-7.0.tar.gz"
   sha256 "3e970203a56a116fe0b136a857b91b6c4f001cb69b6ac8f68bd865bb7c688542"
+  revision 1
 
   depends_on "pkg-config" => :build
-  depends_on "fgsl"
+  depends_on "easyrng"
   depends_on "gcc"
   depends_on "glib"
-  depends_on "gsl"
   depends_on "hdf5"
   depends_on "libxml2"
   depends_on "libxslt"
   depends_on "xraylib"
+  depends_on "libsoup"
+  depends_on "gtkmm-plplot"
 
   def install
     ENV.deparallelize # fortran modules don't like parallel builds
@@ -20,7 +22,7 @@ class XmiMsim < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--disable-gui",
+                          "--enable-gui",
                           "--disable-updater",
                           "--disable-mac-integration",
                           "--disable-libnotify"
