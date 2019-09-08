96a340b07e45b7afe127f24c89d6da0a66933cac
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-09-08 09:50:40 +0100

    fgsl: add patch for removing gsl_spmatrix_compare_idx

--- fgsl.rb
+++ fgsl.rb
@@ -3,10 +3,16 @@ class Fgsl < Formula
   homepage "https://www.lrz.de/services/software/mathematik/gsl/fortran/"
   url "https://doku.lrz.de/download/attachments/28051060/fgsl-1.3.0.tar.gz"
   sha256 "e4ece7600073be16a08bd3450629c2b1198716505cf5fd3365062e0b5c53a142"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "gcc"
   depends_on "gsl"
+
+  patch do
+    url "https://github.com/reinh-bader/fgsl/commit/c306e9f936983df5bab68f8ba55006c0f88bc775.diff"
+    sha256 "7d826e7d1c28791f5f314be98976e664196ea11182cfb6d3e059ac33828c587b"
+  end
 
   def install
     ENV.deparallelize
