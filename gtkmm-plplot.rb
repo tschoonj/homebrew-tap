5a86b22aab53683578f50b6f507d3ba4c078056f
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-12-03 10:27:06 +0000

    gtkmm-plplot: add HEAD

--- gtkmm-plplot.rb
+++ gtkmm-plplot.rb
@@ -3,6 +3,7 @@ class GtkmmPlplot < Formula
   homepage "https://tschoonj.github.io/gtkmm-plplot/"
   url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.3/gtkmm-plplot-2.3.tar.gz"
   sha256 "db2415199b19f3c796000071ba63c3a9e979b8c62a4188777dc06bdee7c44d5d"
+  head "https://github.com/tschoonj/gtkmm-plplot.git"
 
   depends_on "pkg-config" => :build
   depends_on "gtkmm3"
