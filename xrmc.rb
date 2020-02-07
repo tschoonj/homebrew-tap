bb1b70412b78903cba9d69e86f34bcad24d2f1ad
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2020-02-07 14:35:48 +0000

    xrmc 6.7.0

--- xrmc.rb
+++ xrmc.rb
@@ -1,9 +1,8 @@
 class Xrmc < Formula
   desc "Monte Carlo simulation of X-ray imaging and spectroscopy experiments"
   homepage "https://github.com/golosio/xrmc"
-  url "https://xrmc.tomschoonjans.eu/xrmc-6.6.0.tar.gz"
-  sha256 "89c2ca22c44ddb3bb15e1ce7a497146722e3f5a0c294618cae930a254cbbbb65"
-  revision 2
+  url "https://xrmc.tomschoonjans.eu/xrmc-6.7.0.tar.gz"
+  sha256 "03d4cbcb33c7fb59bd9397ad58524687ef7b05bd62dd67fc0b36497ba47d8ccf"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -13,24 +12,11 @@ class Xrmc < Formula
   depends_on "llvm"
   depends_on "xmi-msim" => :optional
 
-  # fix rng
-  patch do
-    url "https://patch-diff.githubusercontent.com/raw/golosio/xrmc/pull/38.patch"
-  end
-
-  # fix clang crashing
-  patch do
-    url "https://github.com/golosio/xrmc/commit/2e12a7daf3f29fdf1f6b2c8d06b86889cf5eaf3f.patch"
-  end
-
   def install
     ENV['CC'] = Formula["llvm"].opt_bin/"clang"
     ENV['CXX'] = Formula["llvm"].opt_bin/"clang++"
     ENV['LDFLAGS'] = "-L#{Formula["llvm"].opt_lib} -Wl,-rpath,#{Formula["llvm"].opt_lib}"
     ENV['CPPFLAGS'] = "-isysroot #{MacOS.sdk_path}"
-
-    inreplace Dir.glob("{examples,test}/*/Makefile.am"),
-      "$(datadir)/examples/xrmc/", "$(datadir)/examples/"
 
     args = %W[
       --disable-dependency-tracking
@@ -47,7 +33,6 @@ class Xrmc < Formula
       args << "--disable-xmi-msim"
     end
 
-    system "autoreconf", "-fiv"
     system "./configure", *args
     system "make"
     system "make", "install"
