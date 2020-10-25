b010ca0aeae41d82eebcdccd4900e1adf64e436d
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-10-25 16:11:20 +0000

    polycap: use python@3.9

--- polycap.rb
+++ polycap.rb
@@ -3,6 +3,7 @@ class Polycap < Formula
   homepage "https://github.com/PieterTack/polycap"
   url "https://github.com/PieterTack/polycap/releases/download/v1.1/polycap-1.1.tar.gz"
   sha256 "4c15c260b00d1c0036df54016e646e25fcc8a333a29ab703a017bd674dadc04b"
+  revision 1
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
@@ -14,9 +15,9 @@ class Polycap < Formula
   depends_on "llvm"
   depends_on "xraylib"
 
-  depends_on "python@3.8" => :recommended
+  depends_on "python@3.9" => :recommended
 
-  if build.with?("python@3.8")
+  if build.with?("python@3.9")
     depends_on "cython" => :build
     depends_on "numpy"
   end
@@ -28,9 +29,9 @@ class Polycap < Formula
 
     args = ["-Dbuild-documentation=false"]
 
-    if build.with?("python@3.8")
+    if build.with?("python@3.9")
       args << "-Dbuild-python-bindings=true"
-      args << "-Dpython=#{Formula["python@3.8"].opt_bin}/python3"
+      args << "-Dpython=#{Formula["python@3.9"].opt_bin}/python3"
     else
       args << "-Dbuild-python-bindings=false"
     end
@@ -48,8 +49,8 @@ class Polycap < Formula
       assert(polycap.__version__ == b"#{version}")
     EOS
 
-    pyversion = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
+    pyversion = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     ENV.prepend_path "PYTHONPATH", lib/"python#{pyversion}/site-packages"
-    system Formula["python@3.8"].opt_bin/"python3", "test.py"
+    system Formula["python@3.9"].opt_bin/"python3", "test.py"
   end
 end
