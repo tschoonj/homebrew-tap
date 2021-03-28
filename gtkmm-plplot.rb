f02a2995093a1e037dd95103e43d0d9f19da446c
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-28 18:05:56 +0100

    gtkmm-plplot: 2.5

--- gtkmm-plplot.rb
+++ gtkmm-plplot.rb
@@ -1,33 +1,26 @@
 class GtkmmPlplot < Formula
   desc "Scientific plotting for Gtkmm3"
   homepage "https://tschoonj.github.io/gtkmm-plplot/"
-  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.4/gtkmm-plplot-2.4.tar.gz"
-  sha256 "f53d2352d6c77b02dbda4d77e18256b843edf70e7405ebf5a06512547eb4c770"
-  revision 3
+  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.5/gtkmm-plplot-2.5.tar.gz"
+  sha256 "9db852627de074bb028045f32f450d4bf14ba968f6bbc0b5c27bac41ae4e1e9e"
 
   head do
     url "https://github.com/tschoonj/gtkmm-plplot.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "mm-common" => :build
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gtkmm3"
   depends_on "plplot"
   depends_on "boost" => :recommended
 
   def install
-    if build.head?
-      system "autoreconf", "-i"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja"
+      system "ninja", "install"
     end
-
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
   end
 
   test do
