99c2a8f6b00dbf747d2ed0ef81ed6522532d3597
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2023-03-10 05:58:32 +0000

    Use python 3.11

--- polycap.rb
+++ polycap.rb
@@ -3,7 +3,7 @@ class Polycap < Formula
   homepage "https://github.com/PieterTack/polycap"
   url "https://github.com/PieterTack/polycap/releases/download/v1.1/polycap-1.1.tar.gz"
   sha256 "4c15c260b00d1c0036df54016e646e25fcc8a333a29ab703a017bd674dadc04b"
-  revision 2
+  revision 3
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
@@ -14,9 +14,9 @@ class Polycap < Formula
   depends_on "llvm"
   depends_on "xraylib"
 
-  depends_on "python@3.10" => :recommended
+  depends_on "python@3.11" => :recommended
 
-  if build.with?("python@3.10")
+  if build.with?("python@3.11")
     depends_on "cython" => :build
     depends_on "numpy"
   end
@@ -28,9 +28,9 @@ class Polycap < Formula
 
     args = ["-Dbuild-documentation=false"]
 
-    if build.with?("python@3.10")
+    if build.with?("python@3.11")
       args << "-Dbuild-python-bindings=true"
-      args << "-Dpython=#{Formula["python@3.10"].opt_bin}/python3"
+      args << "-Dpython=#{Formula["python@3.11"].opt_bin}/python3.11"
     else
       args << "-Dbuild-python-bindings=false"
     end
@@ -48,7 +48,7 @@ class Polycap < Formula
       assert(polycap.__version__ == b"#{version}")
     EOS
 
-    pyversion = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
+    pyversion = Language::Python.major_minor_version Formula["python@3.11"].opt_bin/"python3"
     ENV.prepend_path "PYTHONPATH", lib/"python#{pyversion}/site-packages"
     system Formula["python@3.10"].opt_bin/"python3", "test.py"
   end
--- xraylib.rb
+++ xraylib.rb
@@ -3,16 +3,17 @@ class Xraylib < Formula
   homepage "https://github.com/tschoonj/xraylib"
   url "https://xraylib.tomschoonjans.eu/xraylib-4.1.3.tar.gz"
   sha256 "f280788e9d2d730a01c1031f41233d9926b67c89e4f3fdd7f43f408bb0d24770"
+  revision 1
 
   depends_on "swig" => :build
   depends_on "gcc"
-  depends_on "python@3.10" => :recommended
+  depends_on "python@3.11" => :recommended
   depends_on "fpc" => :optional
   depends_on "lua" => :optional
   depends_on "perl" => :optional
   depends_on "ruby" => :optional
 
-  if build.with?("python@3.10")
+  if build.with?("python@3.11")
     depends_on "cython" => :build
     depends_on "numpy"
   end
@@ -32,10 +33,10 @@ class Xraylib < Formula
     args << (build.with?("ruby") ? "--enable-ruby" : "--disable-ruby")
     args << (build.with?("fpc") ? "--enable-pascal" : "--disable-pascal")
 
-    if build.with?("python@3.10")
+    if build.with?("python@3.11")
       args << "--enable-python"
       args << "--enable-python-numpy"
-      args << "PYTHON=#{Formula["python@3.10"].opt_bin}/python3"
+      args << "PYTHON=#{Formula["python@3.11"].opt_bin}/python3.11"
     else
       args << "--disable-python"
       args << "--disable-python-numpy"
