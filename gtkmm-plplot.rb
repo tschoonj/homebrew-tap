cd32fa1022c2fb126741292bfec65a349e23e2d1
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2024-08-10 14:14:02 +0200

    gtkmm-plplot: bump to 3.0 (gtkmm4 support!)

--- gtkmm-plplot.rb
+++ gtkmm-plplot.rb
@@ -1,8 +1,8 @@
 class GtkmmPlplot < Formula
-  desc "Scientific plotting for Gtkmm3"
+  desc "Scientific plotting for Gtkmm4"
   homepage "https://tschoonj.github.io/gtkmm-plplot/"
-  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-2.5/gtkmm-plplot-2.5.tar.gz"
-  sha256 "9db852627de074bb028045f32f450d4bf14ba968f6bbc0b5c27bac41ae4e1e9e"
+  url "https://github.com/tschoonj/gtkmm-plplot/releases/download/gtkmm-plplot-3.0/gtkmm-plplot-3.0.tar.xz"
+  sha256 "5e345a97abdcde08dd74b5a43d7f07840c0e082537cdc0345f60a02e4b700bfc"
 
   head do
     url "https://github.com/tschoonj/gtkmm-plplot.git"
@@ -11,7 +11,7 @@ class GtkmmPlplot < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "gtkmm3"
+  depends_on "gtkmm4"
   depends_on "plplot"
   depends_on "boost" => :recommended
 
