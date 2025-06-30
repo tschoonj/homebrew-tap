c939bc760668488a97ac94013a1c1796227700f5
Tom Schoonjans <Tom.Schoonjans@genomics.com>
2025-06-30 20:04:49 +0100

    easyrng: use gcc15

--- easyrng.rb
+++ easyrng.rb
@@ -3,7 +3,7 @@ class Easyrng < Formula
   homepage "https://tschoonj.github.io/easyRNG"
   url "https://github.com/tschoonj/easyRNG/releases/download/easyRNG-1.2/easyRNG-1.2.tar.gz"
   sha256 "56259ae12ebb9133e55783b24b8d1a70a59df47f5b1f1485fd689a014b964072"
-  revision 7
+  revision 8
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
@@ -11,9 +11,9 @@ class Easyrng < Formula
 
   def install
     gcc = Formula["gcc"]
-    ENV['CC'] = "#{gcc.opt_bin}/gcc-14"
-    ENV['CXX'] = "#{gcc.opt_bin}/g++-14"
-    ENV['FC'] = "#{gcc.opt_bin}/gfortran-14"
+    ENV['CC'] = "#{gcc.opt_bin}/gcc-15"
+    ENV['CXX'] = "#{gcc.opt_bin}/g++-15"
+    ENV['FC'] = "#{gcc.opt_bin}/gfortran-15"
     ENV.append "LDFLAGS", "-Wl,-ld_classic" if DevelopmentTools.clang_build_version >= 1500
 
     mkdir "build" do
