20c59af3fde3ea0f88168b8e84964053c838dcbd
Tom Schoonjans <Tom.Schoonjans@genomics.com>
2025-06-30 20:05:12 +0100

    polycap: use python 3.13

--- polycap.rb
+++ polycap.rb
@@ -3,6 +3,7 @@ class Polycap < Formula
   homepage "https://github.com/PieterTack/polycap"
   url "https://github.com/PieterTack/polycap/releases/download/v1.2/polycap-1.2.tar.xz"
   sha256 "6e08a8f5ae29f7948a10aa75806bbf11a401ac735de903d4c574b22c25b18dce"
+  revision 1
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
@@ -13,9 +14,9 @@ class Polycap < Formula
   depends_on "llvm"
   depends_on "xraylib"
 
-  depends_on "python@3.12" => :recommended
+  depends_on "python@3.13" => :recommended
 
-  if build.with?("python@3.12")
+  if build.with?("python@3.13")
     depends_on "cython" => :build
     depends_on "numpy"
   end
@@ -27,9 +28,9 @@ class Polycap < Formula
 
     args = ["-Dbuild-documentation=false"]
 
-    if build.with?("python@3.12")
+    if build.with?("python@3.13")
       args << "-Dbuild-python-bindings=true"
-      args << "-Dpython=#{Formula["python@3.12"].opt_bin}/python3.12"
+      args << "-Dpython=#{Formula["python@3.13"].opt_bin}/python3.13"
     else
       args << "-Dbuild-python-bindings=false"
     end
@@ -47,8 +48,8 @@ class Polycap < Formula
       assert(polycap.__version__ == b"#{version}")
     EOS
 
-    pyversion = Language::Python.major_minor_version Formula["python@3.12"].opt_bin/"python3"
+    pyversion = Language::Python.major_minor_version Formula["python@3.13"].opt_bin/"python3"
     ENV.prepend_path "PYTHONPATH", lib/"python#{pyversion}/site-packages"
-    system Formula["python@3.12"].opt_bin/"python3", "test.py"
+    system Formula["python@3.13"].opt_bin/"python3", "test.py"
   end
 end
